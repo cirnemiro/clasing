a84cab2b87745ea164d21527c18550bb5627e8f5
enric <cirnemiro@gmail.com>
2023-10-05 16:10:34 +0200

    table done

--- components/table.tsx
+++ components/table.tsx
@@ -3,28 +3,70 @@
 import { fetchUsers } from "@/data/users";
 import { useQuery } from "@tanstack/react-query";
 import { useEffect, useState } from "react";
+import { useDebounce } from "react-use";
 
 const Table = () => {
 
+    const [results, setResults] = useState<any>(null)
     const [order, setOrder] = useState<boolean>(false)
+    const [querySearch, setQuerySearch] = useState<string>("")
+    const [searchString, setSearchString] = useState<string>("")
 
 
-    const { data, isLoading, isFetching, error } = useQuery({
-        queryKey: ["user",order],
+     const { data, isLoading, isFetching, error } = useQuery({
+        queryKey: ["user",order,querySearch,{staleTime:60}],
         queryFn: fetchUsers,
-    });            
+    });  
 
-    if (order) {
-        data?.results.sort((a:any, b:any) => a.location.state.localeCompare(b.location.state));
+    useDebounce(() => setQuerySearch(searchString), 400, [searchString]);
+
+    console.log(results);
+    
+
+    useEffect(()=>{
+        if (data) {
+            let filteredData = data.results
+            if (order) {
+                filteredData = filteredData.sort((a:any, b:any) => a.location.state.localeCompare(b.location.state))
+            }
+            if (querySearch !== "") {
+                console.log(filteredData.filter((user:any) => user.location.state.toLowerCase().startsWith(querySearch)));
+                
+                filteredData = filteredData.filter((user:any) => user.location.state.toLowerCase().startsWith(querySearch))
+            }
+            setResults(filteredData)
+        }
+    },[data])  
+
+
+    const removeUser = (index:any) => {
+        setResults([
+            ...results.slice(0, index),
+            ...results.slice(index + 1, results.length)
+        ]);
     }
-      
+
+    const resetUsers = () => {
+        let filteredData = data.results
+        if (order) {
+            filteredData = filteredData.sort((a:any, b:any) => a.location.state.localeCompare(b.location.state))
+        }
+        if (querySearch !== "") {
+            console.log(filteredData.filter((user:any) => user.location.state.toLowerCase().startsWith(querySearch)));
+            
+            filteredData = filteredData.filter((user:any) => user.location.state.toLowerCase().startsWith(querySearch))
+        }
+        setResults(filteredData)
+     
+    }
+
     return(
         <div>
             <div>
                 <button>Colorear filas</button>
                 <button onClick={()=>{setOrder(!order)}}>Ordenar por país</button>
-                <button>Resetear estado</button>
-                <input type="text" />
+                <button onClick={()=>{resetUsers()}}>Resetear estado</button>
+                <input type="text" onChange={(e)=>{setSearchString(e.target.value)}}/>
             </div>
             <table>
                 <tr>
@@ -44,17 +86,19 @@ const Table = () => {
                         <td>loading...</td>
                     </tr>
                     :
-                    data.results.map((user:any)=>{
+                    results?.length !== 0 ?
+                    results?.map((user:any,index:number)=>{
                         return(
                             <tr>
                                 <td><img src={user.picture.medium}/></td>
                                 <td>{user.name.first}</td>
                                 <td>{user.name.last}</td>
                                 <td>{user.location.state}</td>
-                                <td><button>borrar</button></td>
+                                <td><button onClick={()=>{removeUser(index)}}>borrar</button></td>
                             </tr>
                         )
-                    })
+                    }):
+                    <>sin resultados</>
                 }
             </table>
         </div>
